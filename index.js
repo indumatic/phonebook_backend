03ada7b7447d95c8bd63b3c91d8854190eec3996
indumatic <electrico@indumatic.net>
2023-12-05 12:20:54 +0100

    uibuild

--- index.js
+++ index.js
@@ -47,25 +47,18 @@ app.get('/api/persons/:id', (request, response, next) => {
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
-
-    if (body.name === undefined) {
-        return response.status(400).json({error: 'name missing'})
-    }
-
-    if (body.number === undefined) {
-        return response.status(400).json({error: 'number missing'})
-    }
+    const {name, number} = request.body
 
     const person = new Person({
-        name: body.name,
-        number: body.number,
+        name: name,
+        number: number,
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
-
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -93,8 +86,6 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-
-
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
@@ -106,6 +97,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({error: 'malformated id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
